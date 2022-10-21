92ba43a5c84ceeaffea34a083675865d704db7e1
stormlei <stormlei.dev@gmail.com>
2022-10-21 20:57:02 +0800

    bug fix

--- app/src/main/java/com/qpsoft/quest/App.kt
+++ app/src/main/java/com/qpsoft/quest/App.kt
@@ -1,10 +1,12 @@
 package com.qpsoft.quest
 
 import android.app.Application
+import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.Utils
 
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
+        LogUtils.getConfig().isLogSwitch = false
     }
 }
